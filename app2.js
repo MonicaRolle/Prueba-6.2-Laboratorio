493b9d0ec8a85b317bcef4c22df0afa4b164bce8
Monica <monica.rolle13@gmail.com>
2021-04-07 13:01:21 +0200

    the optional exercise is missing

--- app2.js
+++ app2.js
@@ -1,3 +1,17 @@
 var randomPick = (n, min, max) => {
+    if (n - 1 > max - min) {
+        console.log("El array es demasiado pequeño")
+    } else {
+        var array = new Array;
+        for (var i = 0; i < n; i++) {
+            const range = max - min + 1;
+            array[i] = Math.floor(Math.random() * range) + min;
+            for (var j = 0; j < i; j++) {
+                if (array[j] === array[i]) i--;
+            }
+        }
+        console.log(array);
+    }
 
 }
+randomPick(10, 1, 10)
